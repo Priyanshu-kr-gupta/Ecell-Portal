a7202ca98500156ad7337a221a6b3d2ec0fa6141
priyanshu kumar <dlnpriyanshu@gmail.com>
2024-11-10 19:02:26 +0530

    get all event and guespeakers endpoints added

--- server/controller/Public.js
+++ server/controller/Public.js
@@ -1,24 +1,26 @@
+const GuestSpeaker = require('../models/GuestSpeaker');
+const Event = require('../models/Event');
 
 const getAllEvent =  async (req,res)=>{
     
-    try{
-
-    }
-    
-    catch(error){
-
-    }
+    try {
+        const events = await Event.find(); 
+        return res.status(200).json(events);
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error while fetching events' });
+      }
 }
 
 
 const getAllGuestSpeakwer =  async (req,res)=>{
-    try{
-
-    }
-    
-    catch(error){
-
-    }
+    try {
+        const guestSpeakers = await GuestSpeaker.find(); 
+        return res.status(200).json(guestSpeakers);
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error while fetching guest speakers' }); 
+      }
 }
 
 module.exports={getAllEvent,getAllGuestSpeakwer}
